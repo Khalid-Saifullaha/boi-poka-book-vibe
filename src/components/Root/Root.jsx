232163f85141d4eab90703c75dffe65d4d1e4f4a
Your Name <khalidsaifullaha3@gmail.com>
2024-10-31 21:14:42 +0600

    feact data and make card

--- src/components/Root/Root.jsx
+++ src/components/Root/Root.jsx
@@ -1,13 +1,11 @@
 import { Outlet } from "react-router-dom";
 import Footer from "../Footer/Footer";
 import NavBar from "../NavBar/NavBar";
-import Banner from "../Banner/Banner";
 
 const Root = () => {
     return (
         <div className="max-w-6xl mx-auto">
             <NavBar></NavBar>
-            <Banner></Banner>
             <Outlet></Outlet>
             <Footer></Footer>
         </div>
